af8ca0950e1ce751efb47876146c4b1980440559
Ahmed Abdul Badie <badie.ahmed@icloud.com>
2015-12-09 23:43:06 +0200

    Modified the Program Parser to handle .ORG and .DATA

--- src/ProgramParser.java
+++ src/ProgramParser.java
@@ -2,86 +2,115 @@ import java.util.Arrays;
 import java.util.regex.*;
 
 public class ProgramParser {
-	private static String jump = "(JMP)\\s+(R[0-7]),\\s+(([0-9]|[1-5][0-9]|6[0-3])|(\\-[0-9]|\\-[1-5][0-9]|\\-6[0-4]))(?:\\s*\\#.*)?";
-	private static String load = "(LW)\\s+(R[1-7]),\\s+(R[0-7]),\\s+(([0-9]|[1-5][0-9]|6[0-3])|(\\-[0-9]|\\-[1-5][0-9]|\\-6[0-4]))(?:\\s*\\#.*)?";
-	private static String store = "(SW)\\s+(R[0-7]),\\s+(R[0-7]),\\s+(([0-9]|[1-5][0-9]|6[0-3])|(\\-[0-9]|\\-[1-5][0-9]|\\-6[0-4]))(?:\\s*\\#.*)?";
-	private static String branch = "(BEQ)\\s+(R[0-7]),\\s+(R[0-7]),\\s+(([0-9]|[1-5][0-9]|6[0-3])|(\\-[0-9]|\\-[1-5][0-9]|\\-6[0-4]))(?:\\s*\\#.*)?";
-	private static String call = "(JALR)\\s+(R[1-7]),\\s+(R[0-7])(?:\\s*\\#.*)?";
-	private static String returnCall = "(RET)\\s+(R[0-7])(?:\\s*\\#.*)?";
-	private static String arithmetic = "(ADD|SUB|NAND|MUL)\\s+(R[1-7]),\\s+(R[0-7]),\\s+(R[0-7])(?:\\s*\\#.*)?";
-	private static String immediate = "(ADDI)\\s+(R[1-7]),\\s+(R[0-7]),\\s+(([0-9]|[1-5][0-9]|6[0-3])|(\\-[0-9]|\\-[1-5][0-9]|\\-6[0-4]))(?:\\s*\\#.*)?";
+	private String origin = "(\\.ORG)\\s+([0-9]|[1-9][0-9]{1,3}|[1-2][0-9]{4}|3[0-2][0-7][0-6][0-7])(?:\\s*\\#.*)?";
+	private String data = "(\\.DATA)\\s+(-([0-9]|[1-9][0-9]{1,3}|[1-2][0-9]{4}|3[0-2][0-7][0-6][0-8])|([0-9]|[1-9][0-9]{1,3}|[1-2][0-9]{4}|3[0-2][0-7][0-6][0-7])),\\s+([0-9]|[1-9][0-9]{1,3}|[1-2][0-9]{4}|3[0-2][0-7][0-6][0-7])(?:\\s*\\#.*)?";
+	private String jump = "(JMP)\\s+(R[0-7]),\\s+(([0-9]|[1-5][0-9]|6[0-3])|(\\-[0-9]|\\-[1-5][0-9]|\\-6[0-4]))(?:\\s*\\#.*)?";
+	private String load = "(LW)\\s+(R[1-7]),\\s+(R[0-7]),\\s+(([0-9]|[1-5][0-9]|6[0-3])|(\\-[0-9]|\\-[1-5][0-9]|\\-6[0-4]))(?:\\s*\\#.*)?";
+	private String store = "(SW)\\s+(R[0-7]),\\s+(R[0-7]),\\s+(([0-9]|[1-5][0-9]|6[0-3])|(\\-[0-9]|\\-[1-5][0-9]|\\-6[0-4]))(?:\\s*\\#.*)?";
+	private String branch = "(BEQ)\\s+(R[0-7]),\\s+(R[0-7]),\\s+(([0-9]|[1-5][0-9]|6[0-3])|(\\-[0-9]|\\-[1-5][0-9]|\\-6[0-4]))(?:\\s*\\#.*)?";
+	private String call = "(JALR)\\s+(R[1-7]),\\s+(R[0-7])(?:\\s*\\#.*)?";
+	private String returnCall = "(RET)\\s+(R[0-7])(?:\\s*\\#.*)?";
+	private String arithmetic = "(ADD|SUB|NAND|MUL)\\s+(R[1-7]),\\s+(R[0-7]),\\s+(R[0-7])(?:\\s*\\#.*)?";
+	private String immediate = "(ADDI)\\s+(R[1-7]),\\s+(R[0-7]),\\s+(([0-9]|[1-5][0-9]|6[0-3])|(\\-[0-9]|\\-[1-5][0-9]|\\-6[0-4]))(?:\\s*\\#.*)?";
 
-	public static void main(String[] args) {
-		System.out.println(Arrays.toString(match("JMP R4, 10")));
-		System.out.println(Arrays.toString(match("LW R1, R5, -11")));
-		System.out.println(Arrays.toString(match("SW R0, R5, -10")));
-		System.out.println(Arrays.toString(match("BEQ R0, R0, -40")));
-		System.out.println(Arrays.toString(match("JALR R1, R5")));
-		System.out.println(Arrays.toString(match("RET R0")));
-		System.out.println(Arrays.toString(match("ADD R1, R6, R0")));
-		System.out.println(Arrays.toString(match("ADDI R3, R5, -40# this is a comment")));
-		System.out.println(Arrays.toString(match("ADDI R3, R5, -40; this is not a comment"))); // returns null
-		System.out.println(Arrays.toString(match("ADDI R3, R5, -65"))); // returns null: Immediate value beyond range [-64, 63]
-		System.out.println(Arrays.toString(match("ADDI R3, R5, 64"))); // returns null: Immediate value beyond range [-64, 63]
-	}
-
-	public static String[] match(String string) {
-		Pattern pattern = Pattern
-				.compile("(JMP|LW|SW|BEQ|JALR|RET|ADDI|ADD|SUB|NAND|MUL).*");
+	public String[] match(String string) {
+		Pattern pattern = Pattern.compile("(\\.ORG|\\.DATA|JMP|LW|SW|BEQ|JALR|RET|ADDI|ADD|SUB|NAND|MUL).*");
 		Matcher matcher = pattern.matcher(string);
 		Matcher m;
 		if (matcher.matches()) {
-			if (matcher.group(1).equals("JMP")) {
+			matcher.group(1);
+			if (matcher.group(1).equals(".ORG")) {
+				m = matcher(string, origin);
+				if (m.matches()) {
+					return new String[] { m.group(1), m.group(2) };
+				}
+			} else if (matcher.group(1).equals(".DATA")) {
+				m = matcher(string, data);
+				if (m.matches()) {
+					return new String[] { m.group(1), m.group(2), m.group(5) };
+				}
+			} else if (matcher.group(1).equals("JMP")) {
 				m = matcher(string, jump);
 				if (m.matches()) {
-					return new String[] {m.group(1), m.group(2), m.group(3)};
+					return new String[] { m.group(1), m.group(2), m.group(3) };
 				}
 			} else if (matcher.group(1).equals("LW")) {
 				m = matcher(string, load);
 				if (m.matches()) {
-					return new String[] {m.group(1), m.group(2), m.group(3), m.group(4)};
+					return new String[] { m.group(1), m.group(2), m.group(3), m.group(4) };
 				}
 			} else if (matcher.group(1).equals("SW")) {
 				m = matcher(string, store);
 				if (m.matches()) {
-					return new String[] {m.group(1), m.group(2), m.group(3), m.group(4)};
+					return new String[] { m.group(1), m.group(2), m.group(3), m.group(4) };
 				}
 			} else if (matcher.group(1).equals("BEQ")) {
 				m = matcher(string, branch);
 				if (m.matches()) {
-					return new String[] {m.group(1), m.group(2), m.group(3), m.group(4)};
+					return new String[] { m.group(1), m.group(2), m.group(3), m.group(4) };
 				}
 			} else if (matcher.group(1).equals("JALR")) {
 				m = matcher(string, call);
 				if (m.matches()) {
-					return new String[] {m.group(1), m.group(2), m.group(3)};
+					return new String[] { m.group(1), m.group(2), m.group(3) };
 				}
 			} else if (matcher.group(1).equals("RET")) {
 				m = matcher(string, returnCall);
 				if (m.matches()) {
-					return new String[] {m.group(1), m.group(2)};
+					return new String[] { m.group(1), m.group(2) };
 				}
 			} else if (matcher.group(1).equals("ADDI")) {
 				m = matcher(string, immediate);
 				if (m.matches()) {
-					return new String[] {m.group(1), m.group(2), m.group(3), m.group(4)};
+					return new String[] { m.group(1), m.group(2), m.group(3), m.group(4) };
 				}
-			} else if (matcher.group(1).equals("ADD")
-					|| matcher.group(1).equals("SUB")
-					|| matcher.group(1).equals("NAND")
-					|| matcher.group(1).equals("MUL")) {
+			} else if (matcher.group(1).equals("ADD") || matcher.group(1).equals("SUB")
+					|| matcher.group(1).equals("NAND") || matcher.group(1).equals("MUL")) {
 				m = matcher(string, arithmetic);
 				if (m.matches()) {
-					return new String[] {m.group(1), m.group(2), m.group(3), m.group(4)};
+					return new String[] { m.group(1), m.group(2), m.group(3), m.group(4) };
 				}
 			}
 		}
 		return null;
 	}
 
-	public static Matcher matcher(String string, String regex) {
+	public Matcher matcher(String string, String regex) {
 		Pattern pattern = Pattern.compile(regex);
 		Matcher matcher = pattern.matcher(string);
 		return matcher;
+	}
+	
+	public static void main(String[] args) {
+		ProgramParser pp = new ProgramParser();
+
+		System.out.println(Arrays.toString(pp.match(".ORG 31765 #$$$$")));
+		System.out.println(Arrays.toString(pp.match(".DATA -32768, 32767 #$#$#$#")));
+		System.out.println(Arrays.toString(pp.match("JMP R4, 10")));
+		System.out.println(Arrays.toString(pp.match("LW R1, R5, -11")));
+		System.out.println(Arrays.toString(pp.match("SW R0, R5, -10")));
+		System.out.println(Arrays.toString(pp.match("BEQ R0, R0, -40")));
+		System.out.println(Arrays.toString(pp.match("JALR R1, R5")));
+		System.out.println(Arrays.toString(pp.match("RET R0")));
+		System.out.println(Arrays.toString(pp.match("ADD R1, R6, R0")));
+		System.out.println(Arrays.toString(pp.match("ADDI R3, R5, 40# this is a comment")));
+		System.out.println(Arrays.toString(pp.match("ADDI R3, R5, -40; this is not a comment"))); // returns
+																									// null
+		System.out.println(Arrays.toString(pp.match("ADDI R3, R5, -65"))); // returns
+																			// null:
+																			// Immediate
+																			// value
+																			// beyond
+																			// range
+																			// [-64,
+																			// 63]
+		System.out.println(Arrays.toString(pp.match("ADDI R3, R5, 64"))); // returns
+																			// null:
+																			// Immediate
+																			// value
+																			// beyond
+																			// range
+																			// [-64,
+																			// 63]
 	}
 }
